b53b3c111566f7f518bae5bcd953274929ee9ab5
Kihog188 <khiemdora@gmail.com>
2025-11-06 14:48:52 +0700

    update dto

---
+++ ev/src/main/java/uth/edu/vn/dto/auth/AuthResponse.java
@@ -0,0 +1,41 @@
+package uth.edu.vn.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Authentication Response DTO
+ * Trả về cho client sau khi đăng nhập/đăng ký thành công
+ * Chứa JWT tokens + thông tin user cơ bản
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+    private Long expiresIn; // seconds
+    
+    // User info
+    private Long userId;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String phoneNumber;
+    
+    /**
+     * Constructor đơn giản chỉ với token
+     * Dùng cho trường hợp chỉ cần trả về token
+     */
+    public AuthResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.tokenType = "Bearer";
+    }
+}
